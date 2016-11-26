f1600aadd888c72a7b7b550bd01cfd907cfb2809
keanpaderes <kean.paderes@gmail.com>
2016-11-26 18:10:09 +0800

    Finished GetSmallestPositive in Simplex Service.

--- app/components/simplex/simplex.js
+++ app/components/simplex/simplex.js
@@ -3,6 +3,8 @@ angular.module('uptimizer.simplex', [])
 .factory('SimplexService', function() {
     var SimplexService = {};
 
+    //All columns are treated as arrays
+
     SimplexService.check = function() {
         console.log("working!");
     };
@@ -18,8 +20,30 @@ angular.module('uptimizer.simplex', [])
         }
         return(count == (column.length -1));
     };
+
+    SimplexService.GetSmallestPositive = function(tr) {
+        var smallest = tr.indexOf(tr.reduce(function (prev, curr) {
+            return (curr > prev) ? curr : prev;
+        }, 0));
+
+        console.log(smallest);
+        for(var i = 0; i<tr.length; i++) {
+            if(tr[i] > 0 && !isNaN(tr[i]) && isFinite(tr[i])) {
+                console.log(tr[i]);
+                if(tr[i] != 0 && tr[i] < tr[smallest])
+                    smallest = i;
+            }
+        }
+        console.log("Smallest " + tr[smallest]);
+        return(smallest);
+    }
+
     SimplexService.simplex = function(tableau) {
-        console.log(SimplexService.IsCleared(SimplexService.getColumn(tableau, 3)));
+        testCol = SimplexService.IsCleared(SimplexService.getColumn(tableau, 3));
+        trSamp = [1.5174, 3.2, 9, NaN];
+        console.log(SimplexService.GetSmallestPositive(trSamp));
+        //Array.apply(null, Array(testCol.length-1)).map(Number.prototype.valueOf,0);
+
         SimplexService.check();
     };
 
