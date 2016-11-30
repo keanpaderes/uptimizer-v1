4b0733d8a05bb3390706f8feaa5d718f06297546
keanpaderes <kean.paderes@gmail.com>
2016-11-30 21:33:25 +0800

    Started parsing constraints.

--- app/components/simplex/simplex-tools.js
+++ app/components/simplex/simplex-tools.js
@@ -41,7 +41,65 @@ angular.module('uptimizer.simplex-tools', [])
     }
 
     //Input handling
-    
+    SimplexTools.createColumnHeaders = function(varList, slackList) {
+        var i = 0;
+        var retArr = [];
+        for(i = 0; i < varList.length; i++){
+            retArr.push(varList[i]);
+        }
+        for(i = 0; i < slackList.length; i++){
+            retArr.push(slackList[i]);
+        }
+        retArr.push("z");
+        retArr.push("ans");
+
+        return retArr;
+    }
+
+    SimplexTools.parseConstraint = function(constraintObject, variableList,
+        numOfConstraints, colNum) {
+        var retArr = [], i = 0;
+        var ans = constraintObject[1];
+
+        // if(constraintObject[0].includes(" + ")) {
+        //     var sC = constraintObject[0].split(" + ");
+        //
+        //     for(i = 0; i < sC.length; i++){
+        //         var spl = sC[i].split(" * ");
+        //
+        //         if(variableList.indexOf(spl[1]) != -1){
+        //             retArr.push(parseInt(spl[0]));
+        //         }
+        //     }
+        //     if(sC.length != variableList.length){
+        //     }
+        // } else {
+        //
+        // }
+        // for(i = 0; i < numOfConstraints; i++){
+        //     if(colNum == i) {
+        //         retArr.push(1);
+        //     } else retArr.push(0);
+        // }
+        return retArr;
+    };
+
+
+    SimplexTools.createTableau = function(valueList,
+        constraintsList, variableList) {
+        var retTab = [];
+        var i = 0, //colNum's +2 is for z and ans
+            colNum = valueList.length + constraintsList.length + 2;
+
+        for(i = 0; i < constraintsList.length; i++){
+            var row = SimplexTools.parseConstraint(
+                constraintsList[i], variableList,
+                constraintsList.length, i
+            );
+            console.log(row);
+            retTab.push(row);
+        }
+    };
 
     return SimplexTools;
 });
--- app/ultop/ultop.js
+++ app/ultop/ultop.js
@@ -13,17 +13,75 @@ angular.module('uptimizer.ultop', [
   });
 }])
 
-.controller('UltimateOptimizerCtrl', ['$scope', 'SimplexService', function($scope, SimplexService) {
-    $scope.objFxn = "";
-    $scope.constraintsList = "";
+.controller('UltimateOptimizerCtrl', ['$scope',
+    'SimplexService', 'SimplexTools',
+    function($scope, SimplexService, SimplexTools) {
 
-    $scope.checkInputs = function() {
+    $scope.inputOF = "";
+    $scope.inputConstraint= "";
+    $scope.objFxn = {
+        "variableList" : [],
+        "valuesList" : []
+    };
+    $scope.constraint = {
+        "slackList" : [],
+        "constraintsList" : []
+    };
+    $scope.isOFDefined = false;
 
+    $scope.defineObjFxn = function() {
+        var oF = $scope.inputOF.split("z =").pop().trim();
+        var oFArray = oF.split(" + ");
+        var returnObject = {
+        };
+
+        for(var i = 0; i < oFArray.length; i++) {
+            var spl = oFArray[i].split(" * ");
+            $scope.objFxn.variableList.push(spl[1]);
+            $scope.objFxn.valuesList.push(parseInt(spl[0])*-1);
+        }
+
+        $scope.isOFDefined = true;
+    };
+
+    $scope.removeObjFxn = function() {
+        $scope.inputOF = "";
+        $scope.objFxn = {
+            "variableList" : [],
+            "valuesList" : []
+        };
+        $scope.constraint = {
+            "slackList" : [],
+            "constraintsList" : []
+        };
+        $scope.isOFDefined = false;
+    };
+
+    $scope.addConstraint = function() {
+        //TODO finalize first if min or max then obj func.
+        $scope.constraint.slackList.push(
+            "s" + ($scope.constraint.constraintsList.length+1));
+        //change into value array
+        //check if max or min then split on that
+        $scope.constraint.constraintsList.push(
+            $scope.inputConstraint.split(" <= "));
+        $scope.inputConstraint= "";
     };
 
     $scope.getInputs = function() {
-        $scope.objFxn = $scope.objFxn.split("z =").pop().trim();
+        var inputTableau = SimplexTools.createTableau(
+            $scope.objFxn.valuesList, $scope.constraint.constraintsList,
+            $scope.objFxn.variableList
+        );
 
+        //define the headers
+        // var columnHeaders =
+        //     SimplexTools.createColumnHeaders(
+        //         $scope.objFxn.variableList, $scope.);
+        //
+        // console.log(columnHeaders);
+
+        //console.log(zInput.split(" + "));
         //to delete!
         var testMatrix = [
             [7, 11, 1, 0, 0, 0, 0, 77],
@@ -32,10 +90,10 @@ angular.module('uptimizer.ultop', [
             [0, 1, 0, 0, 0, 1, 0, 6],
             [-150, -175, 0, 0, 0, 0, 1, 0]
         ];
-        var returnObject = SimplexService.simplex(testMatrix, ["x1", "x2", "s1", "s2", "s3", "s4","z","ans"]);
-        
-        for(var i=0; i<returnObject.tableau.length; i++){
-            console.log(returnObject.tableau[i]);
-        }
+        // var returnObject = SimplexService.simplex(testMatrix, ["x1", "x2", "s1", "s2", "s3", "s4","z","ans"]);
+
+        // for(var i=0; i<returnObject.tableau.length; i++){
+        //     console.log(returnObject.tableau[i]);
+        // }
    }
 }]);
